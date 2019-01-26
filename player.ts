4140e86f23d79e6e86ade8fc7e2aa0fd43de2aa8
Chris Rabl <rabl.christopher@gmail.com>
2019-01-26 11:08:57 -0700

    don't paint trails

--- player.ts
+++ player.ts
@@ -54,9 +54,9 @@ export class Player {
     context.translate(this.position.x, this.position.y);
     context.rotate(this.theta);
     
-    context.strokeStyle = 'blue';
-    context.arc(0, 0, this.radius, 0, 2 * Math.PI);
-    context.stroke();
+    // context.strokeStyle = 'blue';
+    // context.arc(0, 0, this.radius, 0, 2 * Math.PI);
+    // context.stroke();
 
     context.fillStyle = 'red';
     context.fillRect(-10, -5, 20, 10);
